18a684db36187281fb1fec56f4c310fb2e7b5cb9
Grant Cohoe <cohoe@csh.rit.edu>
2012-07-06 15:43:38 -0400

    more dns css fixes

--- application/libraries/core/ImpulseController.php
+++ application/libraries/core/ImpulseController.php
@@ -221,127 +221,116 @@ class ImpulseController extends CI_Controller {
 		return $content;
 	}
 
+	private function _renderDnsTableButtons($viewLink, $modifyLink, $removeLink) {
+		$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
+		$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
+		$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+		return $actions;
+	}
+
 	protected function _renderDnsTable($recs, $header) {
+		$ttlHead = "<th style=\"width: 3em\">TTL</th>";
+		$portHead= "<th style=\"width: 3em\">Port</th>";
+		$weightHead= "<th style=\"width: 3.6em\">Weight</th>";
+		$priorityHead= "<th style=\"width: 3.6em\">Priority</th>";
+		$typeHead= "<th style=\"width: 3.6em\">Type</th>";
 		$counter = 0;
 		$table = "<a name=\"$header\"></a>";
 		$table .= "<table class=\"table table-striped table-bordered imp-dnstable\">";
+		$table .= "<div class=\"imp-dnsheader\"><h3>$header</h3></div>";
 		switch($header) {
 			case "Zone A/AAAA":
-				$table .= "<tr><td colspan=5><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Zone</th><th>TTL</th><th>Type</th><th>Address</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Zone</th><th style=\"width: 9%\">TTL</th><th>Type</th><th>Address</th><th style=\"width: 162px;\">Actions</th></tr>";
 				foreach($recs as $aRec) {
 					if(get_class($aRec) != "ZoneAddressRecord") { continue; }
 					$viewLink = "/dns/zonea/view/".rawurlencode($aRec->get_zone())."/".rawurlencode($aRec->get_address());
 					$modifyLink = "/dns/zonea/modify/".rawurlencode($aRec->get_zone())."/".rawurlencode($aRec->get_address());
 					$removeLink = "/dns/zonea/remove/".rawurlencode($aRec->get_zone())."/".rawurlencode($aRec->get_address());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$aRec->get_zone()}</td><td>{$aRec->get_ttl()}</td><td>{$aRec->get_type()}</td><td>{$aRec->get_address()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "A/AAAA":
-				$table .= "<tr><td colspan=6><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Hostname</th><th>Zone</th><th>TTL</th><th>Type</th><th>Owner</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Hostname</th><th>Zone</th>$ttlHead$typeHead<th>Owner</th><th style=\"width: 162px; max-width: 50px;\">Actions</th></tr>";
 				foreach($recs as $aRec) {
 					if(get_class($aRec) != "AddressRecord") { continue; }
 					$viewLink = "/dns/a/view/".rawurlencode($aRec->get_zone())."/".rawurlencode($aRec->get_address());
 					$modifyLink = "/dns/a/modify/".rawurlencode($aRec->get_zone())."/".rawurlencode($aRec->get_address());
 					$removeLink = "/dns/a/remove/".rawurlencode($aRec->get_zone())."/".rawurlencode($aRec->get_address());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$aRec->get_hostname()}</td><td>{$aRec->get_zone()}</td><td>{$aRec->get_ttl()}</td><td>{$aRec->get_type()}</td><td>{$aRec->get_owner()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "CNAME":
-				$table .= "<tr><td colspan=7><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Alias</th><th>Hostname</th><th>Zone</th><th>TTL</th><th>Type</th><th>Owner</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Alias</th><th>Hostname</th><th>Zone</th><th style=\"width: 9%\">TTL</th><th>Type</th><th>Owner</th><th style=\"width: 162px\">Actions</th></tr>";
 				foreach($recs as $cRec) {
 					if(get_class($cRec) != "CnameRecord") { continue; }
 					$viewLink = "/dns/cname/view/".rawurlencode($cRec->get_zone())."/".rawurlencode($cRec->get_alias());
 					$modifyLink = "/dns/cname/modify/".rawurlencode($cRec->get_zone())."/".rawurlencode($cRec->get_alias());
 					$removeLink = "/dns/cname/remove/".rawurlencode($cRec->get_zone())."/".rawurlencode($cRec->get_alias());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$cRec->get_alias()}</td><td>{$cRec->get_hostname()}</td><td>{$cRec->get_zone()}</td><td>{$cRec->get_ttl()}</td><td>{$cRec->get_type()}</td><td>{$cRec->get_owner()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "SRV":
-				$table .= "<tr><td colspan=10><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Alias</th><th>Hostname</th><th>Zone</th><th>Priority</th><th>Weight</th><th>Port</th><th>TTL</th><th>Type</th><th>Owner</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Alias</th><th>Hostname</th><th>Zone</th>$priorityHead$weightHead$portHead$ttlHead$typeHead<th>Owner</th><th style=\"width: 162px\">Actions</th></tr>";
 				foreach($recs as $sRec) {
 					if(get_class($sRec) != "SrvRecord") { continue; }
 					$viewLink = "/dns/srv/view/".rawurlencode($sRec->get_zone())."/".rawurlencode($sRec->get_alias())."/".$sRec->get_priority()."/".$sRec->get_weight()."/".$sRec->get_port();
 					$modifyLink = "/dns/srv/modify/".rawurlencode($sRec->get_zone())."/".rawurlencode($sRec->get_alias())."/".$sRec->get_priority()."/".$sRec->get_weight()."/".$sRec->get_port();
 					$removeLink = "/dns/srv/remove/".rawurlencode($sRec->get_zone())."/".rawurlencode($sRec->get_alias())."/".$sRec->get_priority()."/".$sRec->get_weight()."/".$sRec->get_port();
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$sRec->get_alias()}</td><td>{$sRec->get_hostname()}</td><td>{$sRec->get_zone()}</td><td>{$sRec->get_priority()}</td><td>{$sRec->get_weight()}</td><td>{$sRec->get_port()}</td><td>{$sRec->get_ttl()}</td><td>{$sRec->get_type()}</td><td>{$sRec->get_owner()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "Zone TXT":
-				$table .= "<div class=\"imp-dnsheader\"><h3>Zone TXT</h3></div>";
-				$table .= "<tr><th style=\"width: 15%\">Hostname</th><th style=\"width: 15%\">Zone</th><th>Text</th><th style=\"width: 9%\">TTL</th><th style=\"width: 9%\">Type</th><th style=\"width: 31%\">Actions</th></tr>";
+				$table .= "<tr><th style=\"width: 15%\">Hostname</th><th style=\"width: 15%\">Zone</th><th>Text</th><th style=\"width: 9%\">TTL</th><th style=\"width: 9%\">Type</th><th style=\"width: 162px\">Actions</th></tr>";
 				foreach($recs as $tRec) {
 					if(get_class($tRec) != "ZoneTextRecord") { continue; }
 					$viewLink = "/dns/zonetxt/view/".rawurlencode($tRec->get_zone())."/".md5($tRec->get_text());
 					$modifyLink = "/dns/zonetxt/modify/".rawurlencode($tRec->get_zone())."/".md5($tRec->get_text());
 					$removeLink = "/dns/zonetxt/remove/".rawurlencode($tRec->get_zone())."/".md5($tRec->get_text());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$tRec->get_hostname()}</td><td>{$tRec->get_zone()}</td><td><div style=\"word-wrap: break-word\">{$tRec->get_text()}</div></td><td>{$tRec->get_ttl()}</td><td>{$tRec->get_type()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "TXT":
-				$table .= "<tr><td colspan=7><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Hostname</th><th>Zone</th><th>Text</th><th>TTL</th><th>Type</th><th>Owner</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Hostname</th><th>Zone</th><th>Text</th><th style=\"width: 9%\">TTL</th><th>Type</th><th>Owner</th><th style=\"width: 162px\">Actions</th></tr>";
 				foreach($recs as $tRec) {
 					if(get_class($tRec) != "TextRecord") { continue; }
 					$viewLink = "/dns/txt/view/".rawurlencode($tRec->get_zone())."/".rawurlencode($tRec->get_hostname())."/".md5($tRec->get_text());
 					$modifyLink = "/dns/txt/modify/".rawurlencode($tRec->get_zone())."/".rawurlencode($tRec->get_hostname())."/".md5($tRec->get_text());
 					$removeLink = "/dns/txt/remove/".rawurlencode($tRec->get_zone())."/".rawurlencode($tRec->get_hostname())."/".md5($tRec->get_text());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$tRec->get_hostname()}</td><td>{$tRec->get_zone()}</td><td>{$tRec->get_text()}</td><td>{$tRec->get_ttl()}</td><td>{$tRec->get_type()}</td><td>{$tRec->get_owner()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "NS":
-				$table .= "<tr><td colspan=6><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Nameserver</th><th>Zone</th><th>TTL</th><th>Type</th><th>Owner</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Nameserver</th><th>Zone</th><th style=\"width: 9%\">TTL</th><th>Type</th><th>Owner</th><th style=\"width: 162px\">Actions</th></tr>";
 				foreach($recs as $nRec) {
 					if(get_class($nRec) != "NsRecord") { continue; }
 					$viewLink = "/dns/ns/view/".rawurlencode($nRec->get_zone())."/".rawurlencode($nRec->get_nameserver());
 					$modifyLink = "/dns/ns/modify/".rawurlencode($nRec->get_zone())."/".rawurlencode($nRec->get_nameserver());
 					$removeLink = "/dns/ns/remove/".rawurlencode($nRec->get_zone())."/".rawurlencode($nRec->get_nameserver());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$nRec->get_nameserver()}</td><td>{$nRec->get_zone()}</td><td>{$nRec->get_ttl()}</td><td>{$nRec->get_type()}</td><td>{$nRec->get_owner()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
 				break;
 			case "MX":
-				$table .= "<tr><td colspan=7><h3>$header</h3></td></tr>";
-				$table .= "<tr><th>Hostname</th><th>Zone</th><th>Preference</th><th>TTL</th><th>Type</th><th>Owner</th><th>Actions</th></tr>";
+				$table .= "<tr><th>Hostname</th><th>Zone</th><th>Preference</th><th style=\"width: 9%\">TTL</th><th>Type</th><th>Owner</th><th style=\"width: 162px\">Actions</th></tr>";
 				foreach($recs as $mRec) {
 					if(get_class($mRec) != "MxRecord") { continue; }
 					$viewLink = "/dns/mx/view/".rawurlencode($mRec->get_zone())."/".rawurlencode($mRec->get_preference());
 					$modifyLink = "/dns/mx/modify/".rawurlencode($mRec->get_zone())."/".rawurlencode($mRec->get_preference());
 					$removeLink = "/dns/mx/remove/".rawurlencode($mRec->get_zone())."/".rawurlencode($mRec->get_preference());
-					$actions = "<a href=\"$removeLink\"><button class=\"btn btn-mini btn-danger pull-right\">Remove</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$modifyLink\"><button class=\"btn btn-mini btn-warning pull-right\">Modify</button></a><span class=\"pull-right\">&nbsp</span>";
-					$actions .= "<a href=\"$viewLink\"><button class=\"btn btn-info btn-mini pull-right\">Details</button></a>";
+					$actions = $this->_renderDnsTableButtons($viewLink, $modifyLink, $removeLink);
 					$table .= "<tr><td>{$mRec->get_hostname()}</td><td>{$mRec->get_zone()}</td><td>{$mRec->get_preference()}</td><td>{$mRec->get_ttl()}</td><td>{$mRec->get_type()}</td><td>{$mRec->get_owner()}</td><td>$actions</td></tr>";
 					$counter++;
 				}
