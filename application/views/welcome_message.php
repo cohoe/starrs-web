55215158b320923ddc086fc64a6643ee165af3c6
Grant Cohoe <cohoe@csh.rit.edu>
2012-07-12 10:59:19 -0400

    welcome

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,17 +5,22 @@
 		<p>IMPULSE lets you manage and allocate network resources in very complex configurations. Manage your systems, interfaces, addresses, and DNS records easily without having to pester your local system administrators.
 	</div>
 	<div class="row-fluid">
-		<div class="span4">
+		<div class="span3">
 			<h2>Quick Start</h2>
 			<p>Use this to get a system registered on the network quickly with a simple configuration</p>
 			<p><a class="btn btn-primary" href="#">Quick Start &raquo;</a></p>
 		</div><!--/span-->
-		<div class="span4">
+		<div class="span3">
 			<h2>Learn More</h2>
 			<p>Read the documentation about the various system configurations that IMPULSE supports and some of the more fancy features that can be of use.</p>
 			<p><a class="btn btn-primary" href="#">Learn More &raquo;</a></p>
 		</div><!--/span-->
-			<div class="span4">
+		<div class="span3">
+			<h2>Setup</h2>
+			<p>For administrators, see how to get started with a basic IMPULSE installation</p>
+			<p><a class="btn btn-primary" href="#">Setup &raquo;</a></p>
+		</div>
+		<div class="span3">
 			<h2>Report a Problem</h2>
 			<p>IMPULSE is by no means perfect. If you find a problem, file a bug on the appropriate Github project:
 			<ul>
