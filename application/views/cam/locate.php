22787157784dcdaced08dd92e917827d3383efc3
Grant Cohoe <cohoe@csh.rit.edu>
2012-07-22 21:23:19 -0400

    more fixes

--- application/views/cam/locate.php
+++ application/views/cam/locate.php
@@ -3,6 +3,6 @@
 		<dt>System</dt>
 		<dd><a href="/system/view/<?=rawurlencode($p['system_name']);?>"><?=$p['system_name'];?></a></dd>
 		<dt>Interface</dt>
-		<dd><?=$p['ifname'];?></dd>
+		<dd><a href="/network/switchports/view/<?=rawurlencode($p['system_name']);?>"><?=$p['ifname'];?></a></dd>
 	</dl>
 </div>
