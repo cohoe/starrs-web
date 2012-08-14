a62509dfa2ca97593a86f3949cf84abf7fe34f01
Grant Cohoe <cohoe@csh.rit.edu>
2012-08-14 01:57:10 -0400

    added https shim

--- application/views/core/main.php
+++ application/views/core/main.php
@@ -54,7 +54,7 @@
 		<?=$scripts;?>
 		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]>
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 			<script src="/js/iefix.js"></script>
 		<![endif]-->
 
