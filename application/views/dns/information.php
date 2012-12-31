6de102e5a033ac717ee8be6e9bd956d4f60bb0f3
Grant Cohoe <cohoe@csh.rit.edu>
2012-12-31 18:12:44 -0500

    fixes #125

--- application/views/dns/information.php
+++ application/views/dns/information.php
@@ -7,7 +7,7 @@
 		Zones (or domains) are areas of the DNS namespace that your records are assigned to. STARRS will manage your entire zone for you and handle all updates dynamically so you never need to directly edit the file.</p>
 		<h2>Keys</h2>
 		In order to perform dynamic updates on a zone, a key is used to authenticate to the DNS server that will receive the update. Every zone that STARRS controls needs a key in order to function properly, otherwise no updates will be sent.</p>
-		<h2>Systems</h2>
-		Click a system to view the addresses and DNS records associated with it. You will be able to create, modify, and remove your records from there.
+		<h2>Addresses</h2>
+		DNS addresses are created on registered addresses. Click an address to view and modify it's respective DNS records.
 	</div>
 </div><!--/span-->
